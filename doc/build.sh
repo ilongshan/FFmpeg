a1abcb4e9cf30dd1a03108145a25fb84105abef6
Longshan Du <dulongshan@fenbi.com>
2020-11-03 10:54:56 +0800

    ADD: add doc/build.sh

---
+++ doc/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+./configure --prefix=/Users/longshan/Work/local --enable-gpl --enable-nonfree --enable-libass  --enable-libfreetype --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libopus --enable-libxvid --samples=fate-suite --disable-asm --disable-optimizations --enable-debug=3 --enable-libtensorflow --enable-linux-perf --extra-cflags=-DTRACE
